c27dbdfefd644ed4ad7d0c227f7032e8b29dbeb6
Vsevolod Volkov <st.lyn4@gmail.com>
2021-04-07 22:46:12 +0300

    Basic info fetch

--- src/screens/Gallery.js
+++ src/screens/Gallery.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ScrollView } from 'react-native';
 import { Text } from 'react-native-paper';
+import axios from 'axios';
+import { UNSPLASH_TOKEN } from '@env';
 
-const Gallery = ({ navigation }) => {
-  return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('Photo', { name: 'Test' })}
-    >
-      <Text>Gallery</Text>
-    </TouchableOpacity>
-  );
-};
+class Gallery extends React.Component {
+  state = {
+    fetched: false,
+    photos: null,
+  };
+
+  componentDidMount = () => {
+    axios
+      .get('https://api.unsplash.com/photos', {
+        params: { client_id: UNSPLASH_TOKEN },
+      })
+      .then(({ data: photos }) => {
+        console.log(photos);
+        this.setState({ fetched: true, photos });
+      })
+      .catch((err) => {
+        console.error('Что-то пошло не так:', err);
+      });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <ScrollView>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Photo', { name: 'Test' })}
+        >
+          <Text>
+            {this.state.fetched
+              ? JSON.stringify(this.state.photos, null, '\t')
+              : 'Fetching...'}
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
+}
 
 export default Gallery;
